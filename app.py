f604f6b6a4ebade177aa9b8e9fcc1b279129f386
Suyog Hole <75977740+suyog56@users.noreply.github.com>
2025-10-08 23:22:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,47 +3,68 @@ import pickle
 import pandas as pd
 import requests
 
-# Fetch poster
-def fetch_poster(movie_id):
-    response = requests.get(
-        f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=8a5b5879a3c61ee2ca3e536245035b10&language=en-US'
-    )
-    data = response.json()
-    poster_path = data.get('poster_path')
-    if poster_path:
-        return "https://image.tmdb.org/t/p/w500/" + poster_path
-    else:
-        return "https://via.placeholder.com/500x750?text=No+Image"
-
-# Load movies and precomputed top similar movies
-movies_dict = pickle.load(open('movies_dict.pkl','rb'))
-movies = pd.DataFrame(movies_dict)
-new_df = movies.copy()
-top_similar = pickle.load(open('similarity.pkl.gz','rb'))  # precomputed top-N
-
+def fetch_poste(moive_id):
+     responce = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8a5b5879a3c61ee2ca3e536245035b10&language=en-US'.format(moive_id))
+     data = responce.json()
+     poster_path = data['poster_path']
+     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+     return full_path
+     #return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+#new_df = movies.copy()
 def recommend(movie):
-    movie_index = new_df[new_df["title"] == movie].index[0]
-    similar_indices = top_similar[movie_index]
-
+    # find the index of the selected movie
+    movie_index = new_df[movies["title"] == movie].index[0]
+    
+    # compute similarity scores
+    distances = similarity[movie_index]
+    
+    # get top 5 similar movies (excluding the first itself)
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    
     recommended_movies = []
     recommended_posters = []
-
-    for i in similar_indices:
-        recommended_movies.append(new_df.iloc[i].title)
-        recommended_posters.append(fetch_poster(new_df.iloc[i].movie_id))
-
+    
+    for i in movie_list:
+        movie_id = new_df.iloc[i[0]].movie_id
+        recommended_movies.append(new_df.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
+    
     return recommended_movies, recommended_posters
 
-# Streamlit UI
-st.title("Movie Recommender System")
-selected_movie_name = st.selectbox('Select the movie', movies['title'].values)
 
+
+movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+st.title("Moive Recommender System")
+#selected_moive_name = st.selectbox('Select the moive?',movies['title'].values)
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
 if st.button('Show Recommendation'):
-    names, posters = recommend(selected_movie_name)
+ names ,posters = recommend(selected_movie)
+ col1, col2, col3, col4, col5 = st.columns(5)
+ with col1:
+     st.text(names[0])
+     st.image(posters[0])
+ with col2:
+     st.text(names[1])
+     st.image(posters[1])
+ with col3:
+     st.text(names[2])
+     st.image(posters[2])
+ with col4:
+     st.text(names[3])
+     st.image(posters[3])
+ with col5:
+    st.text(names[4])
+    st.image(posters[4])
 
-    # Dynamic columns for any number of recommendations
-    cols = st.columns(len(names))
-    for col, name, poster in zip(cols, names, posters):
-        with col:
-            st.text(name)
-            st.image(poster)
+
+
+
+
+
