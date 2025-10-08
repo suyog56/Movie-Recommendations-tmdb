9d8665bf1762a9ce8c711f3e8cfde45fc583d241
Suyog Hole <75977740+suyog56@users.noreply.github.com>
2025-10-08 23:05:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def fetch_poste(moive_id):
 #new_df = movies.copy()
 def recommend(movie):
     # find the index of the selected movie
-    movie_index = new_df[movies["title"] == movie].index[0]
+    movie_index = new_df[movies["title"] == movie].index[0]]
     
     # compute similarity scores
     distances = similarity[movie_index]
@@ -62,5 +62,6 @@ if st.button('Show Recommendation'):
  with col5:
     st.text(names[4])
     st.image(posters[4])
+
 
 
