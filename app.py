6a70a874c327eea0d44d8b786b1c4a9a72a077d6
Suyog Hole <75977740+suyog56@users.noreply.github.com>
2025-10-08 22:37:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,24 +43,25 @@ selected_moive_name = st.selectbox('Select the moive?',movies['title'].values)
 if st.button('Recommend'):
  names ,posters = recommend(selected_movie_name)
  col1, col2, col3, col4, col5 = st.beta_columns(5)
-  with col1:
+ with col1:
     st.text(names[0])
     st.image(posters[0])
-  with col2:
+ with col2:
     st.text(names[1])
     st.image(posters[1])
-  with col3:
+ with col3:
     st.text(names[2])
     st.image(posters[2])
-  with col4:
+ with col4:
     st.text(names[3])
     st.image(posters[3])
-  with col5:
+ with col5:
     st.text(names[4])
     st.image(posters[4])
 
  
 	
+
 
 
 
